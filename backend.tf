be8a9322fe1bbad9a374523c3c4db3aba0d04169
Tun Nyein <tunnyein.ict@gmail.com>
2025-08-10 06:56:16 +0000

    Added first commit

--- backend.tf
+++ backend.tf
@@ -2,11 +2,11 @@
 terraform {
   backend "remote" {
     hostname = "app.terraform.io"
-    organization = "self-driven-labs"
+    organization = "hc-global"
 
     workspaces {
-      project = "tf-hcp-aws-vcs-lab01"
-      name = "tf-hcp-aws-vcs-workflow"
+      project = "hc-global"
+      name = "tf-lab03-vcs-ws"
       
     }
   }
